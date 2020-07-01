b5d8fbc2ff56e98ae5133a2e7827f80de164aac9
Mohammed Bashir <mohammed.ee@hotmail.com>
2020-07-01 14:30:02 +0300

    first commit

---
+++ Asymmetric/RSA.py
@@ -0,0 +1,19 @@
+from Crypto.PublicKey import RSA
+from hashlib import sha512
+
+keyPair = RSA.generate(bits=1024)
+print(f"Public key: (n={hex(keyPair.n)}, e={hex(keyPair.e)})")
+print(f"Private key: (n={hex(keyPair.n)}, e={hex(keyPair.d)})")
+
+#sign
+msg = b'Golden Chip Company'
+hash = int.from_bytes(sha512(msg).digest(), byteorder='big')
+signature = pow(hash, keyPair.d, keyPair.n)
+print(hash)
+print("Signature: ", hex(signature))
+
+msg = b'Golden Chip Company'
+# RSA verify signature
+hash = int.from_bytes(sha512(msg).digest(), byteorder='big')
+hashFromSignature = pow(signature, keyPair.e, keyPair.n)
+print("Signature valid:", hash == hashFromSignature)
